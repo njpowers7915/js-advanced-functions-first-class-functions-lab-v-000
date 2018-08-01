00fc10c17b0af01adb5174476e390e8fe3355c1d
Nicholas Powers <njpowers7915@gmail.com>
2018-08-01 12:08:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,3 +17,7 @@ const createFareMultiplier = function (integer) {
 
 const fareDoubler = createFareMultiplier(2);
 const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(arrayOfDrivers, function) {
+  
+}
